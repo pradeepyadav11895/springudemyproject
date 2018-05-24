2eb81b829cd9c8cb1dfb9f69246e78245e08a765
pradeepyadav11895 <er.pradeepy118@gmail.com>
2018-05-24 17:33:55 +0530

    initail commit for boot project

---
+++ src/main/java/com/bluebird/BluebirdApplication.java
@@ -0,0 +1,12 @@
+package com.bluebird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluebirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluebirdApplication.class, args);
+	}
+}
